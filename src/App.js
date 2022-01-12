f35d65f88ba884233f87652613f16e11ee06b4c3
Felipe <fpizarro.ux@gmail.com>
2022-01-12 19:13:49 +0100

    settingupStrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React from "react";
 
 function App() {
-  return <div>Hello Felipón!</div>;
+  return (
+  <div>
+    <div className="container">
+      <div className="row mt-5" >
+          <div className="col-6 m-auto">
+            Hello I'm Felipe
+          </div>
+          <div className="col-6 m-auto">
+            Projects
+          </div>
+      </div>
+    </div>
+  </div>
+  )
 }
 
 export default App;
