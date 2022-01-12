7e6dd327da5089b866703421b270b5362b949e05
Felipe <fpizarro.ux@gmail.com>
2022-01-12 19:32:28 +0100

    managingHome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
-function App() {
+
+class App extends Component {
+  render() {
+      return (
+          <div className="App">
+              <Navbar dark color="primary">
+              <div className="container">
+                  <NavbarBrand href="/">NuCamp</NavbarBrand>
+              </div>
+              </Navbar>
+          </div>
+      );
+  }
+}
+
+function hellofelipe() {
   return (
   <div>
     <div className="container">
