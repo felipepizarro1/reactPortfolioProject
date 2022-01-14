a734526fe02bf416ce7861804b911e480540cd26
Felipe <fpizarro.ux@gmail.com>
2022-01-14 18:42:13 +0100

    homeUpdate

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,15 +6,14 @@ function Homecomponent() {
             <div class="container ">
                 <div class="row m-5" >
                     <div class="col-sm-6 mt-5" data-aos="fade-right">
-                        <h1 >Hello there! I'm Felipe</h1>
-                        <h2 class="d-none d-sm-block">Full Stack Developer Student</h2>
-                        <button type="button" data-toggle="modal" data-target="#adoptapet" class="btn mt-4  btn-lg btn-outline-light">Contact</button>
-
+                        <h1 >Hello There!<h1 className="textstrong">I'm Felipe Pizarro</h1></h1>
+                        <p className="d-none d-sm-block ">Full Stack Developer Student at <br></br>Nucamp Coding <em className='textstrong'>Bootcamp</em></p>
+                        <a href="mailto:fpizarro.ux@gmail.com"><button type="button"  data-toggle="modal" data-target="#adoptapet" class="btn mt-4  btn-lg btn-outline-light">Contact</button></a>
                     </div>
                     <div class="col-6 mt-2">
                         <h1>Current Projects</h1>
         
-                        <a href='https://argoswebsite.netlify.app/' target="_blank"><h3>‣ Argos Website: Pet Finder Project</h3></a>
+                        <a href='https://argoswebsite.netlify.app/' target="_blank" style={{ textDecoration: 'none' }}><h3>‣ Argos: PetFinder Redesign Project</h3></a>
                     </div>
                 </div>
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -15,21 +15,24 @@ function Headercomponent () {
             <div className="row">
                 <div className="col mt-5 ml-5 col-sm-5 ">
                     <div className='container mt-3 d-flex'>
-                        <a className="btn btn-social-icon btn-github" href="https://github.com/felipepizarro1" target="_blank"><i className="fa fa-github" /></a>{' '}
-                        <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/felipepizarrodev" target="_blank"><i className="fa fa-linkedin" /></a>{' '}
-                    </div>
+                        <a className="btn btn-social-icon " href="https://github.com/felipepizarro1" target="_blank"><i className="fa fa-github" /></a>{' '}
+                        <a className="btn btn-social-icon ml-1" href="https://www.linkedin.com/in/felipepizarrodev" target="_blank"><i className="fa fa-linkedin" /></a>{' '}
+                     </div>
                 </div>
 
                 
                 <div className="col mt-5">
                     <div className="container d-flex">
                         <nav className="navbar navbar-expand-md what ml-auto">
-                            <a className="navbar-brand" href="#">Portfolio</a>
+                            
                             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                             </button>
                             <div className="collapse navbar-collapse" id="navbarNav">
                                 <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                    <a className="nav-link" href="#">Portfolio</a>
+                                    </li>
                                     <li className="nav-item">
                                     <a className="nav-link" type="button" id="button">About <span className="sr-only">(current)</span></a>
                                     </li>
