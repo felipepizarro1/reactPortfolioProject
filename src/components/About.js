80919f0850b126f84d82d0adfa8e0ea4f7b273d1
Felipe <fpizarro.ux@gmail.com>
2022-02-17 18:49:59 +0100

    textUpdate

--- src/components/About.js
+++ src/components/About.js
@@ -13,19 +13,20 @@ function Aboutcomponent () {
                 exitTransform: 'scale(0.9) translateY(60%)'
             }}>
                 <div class="container ">
-                    <div className="row m-5" >
+                    <div className="row mt-5 mr-5 ml-5 mb-2" >
                         <div className="col-sm-10 mt-2">
                             <h1 className="textstrong mb-4"> About Me</h1>
-                            <p className='d-sm-block '>I'm Felipe Pizarro, a graduate of Social Communications and Professional Journalism of the UNIACC University of Santiago de Chile. During my fourth year of studying, I was the winner of the Santander Mobility Scholarship (2016) that allowed me to study for a semester at the University of Seville in Spain in 2017. As a student I was also a Host for a TV pilot and I was a Contributor to a Newspaper.</p>  
+                            <p className='d-sm-block '>Hi, I'm Felipe, a graduate of Full Stack Development from Nucamp Coding Bootcamp based in Milan, Italy. In 2020, I graduated in communication sciences in Chile, at the UNIACC University and worked as a journalist for a year and a half doing scientific dissemination at the ALMA Observatory and as a video editor at private E-learning agencies.</p>  
                             
                         </div>
                         <div className="col-sm-2 mt-2">
                              <img  width={200} src={imgfelipe} />
                         </div>
                     </div>
-                    <div className="row m-5" >
+                    <div className="row mt-2 mr-5 ml-5 mb-2" >
                         <div class="col-sm-12" data-aos="fade-right">
-                            <p className="d-sm-block ">I have 2 years of experience in creating digital content. I worked as an intern and later as a Video Editor at the ALMA Observatory, disseminating scientific knowledge in an audiovisual format. I also worked in a private organization  called CCDO, where I created E-learning courses to train both workers and entrepreneurs of different backgrounds .<em className='textstrong'>Bootcamp</em></p>
+                            <p className="d-sm-block ">However, it was due to the pandemic that it was difficult for me to continue to be involved in my work in communications. This is how I came to the conclusion of making a career change towards the technological area, more specifically in the area of ​​software development, a personal interest that I always had but had not projected as serious until that moment.<em className='textstrong'>Bootcamp</em></p>
+                            <p className="d-sm-block ">While moving to Italy for family reasons, I started studying the basics of UX/UI through Georgia Tech's professional Human Computer Interaction program and HTML/CSS through the many tutorials on Youtube and FreeCodeCamp. I also polished my graphic design skills with UI prototypes in Photoshop and Adobe XD.<em className='textstrong'>Bootcamp</em></p>
                         </div>
                     </div>
                 </div>
--- src/components/Project1.js
+++ src/components/Project1.js
@@ -19,19 +19,25 @@ class Project1 extends Component {
                     <FadeTransform
                     in
                     transformProps={{
-                        exitTransform: 'scale(0.9) translateY(60%)'
+                        exitTransform: 'scale(0.1) translateY(90%)'
                     }}>
-                        <h1 >Argos Website<h1 className="textstrong"></h1></h1>
+                        <h1 >Argos Website<h1 className="textstrong">What</h1></h1>
                         <p className="d-sm-block ">Website with Scrolling FX <br></br>Nucamp Coding <em className='textstrong'>Bootcamp</em></p>
                         <a href="mailto:fpizarro.ux@gmail.com"><button type="button"  data-toggle="modal" data-target="#adoptapet" class="btn mt-4 mr-2 btn-lg btn-outline-light">View Live</button></a>
                         <a href="https://github.com/felipepizarro1/argos-website" target="_blank"><button type="button"  data-toggle="modal" data-target="#adoptapet" class="btn mt-4  btn-lg btn-outline-light">View Code</button></a>
                     </FadeTransform>
                     </div>
                     <div class="col-sm-7 col-12">
+                    <Stagger in>
+                            <Fade in>  
 
                             <p>I'm Felipe Pizarro, a graduate of Social Communications and Professional Journalism of the UNIACC University of Santiago de Chile. During my fourth year of studying, I was the winner of the Santander Mobility Scholarship (2016) that allowed me to study for a semester at the University of Seville in Spain in 2017. As a student I was also a Host for a TV pilot and I was a Contributor to a Newspaper.</p>
+                            </Fade>
+                            <Fade in>  
 
                             <p>I have 2 years of experience in creating digital content. I worked as an intern and later as a Video Editor at the ALMA Observatory, disseminating scientific knowledge in an audiovisual format. I also worked in a private organization  called CCDO, where I created E-learning courses to train both workers and entrepreneurs of different backgrounds .</p>
+                            </Fade>
+                        </Stagger>
                     </div>
                 </div>
             </div>
