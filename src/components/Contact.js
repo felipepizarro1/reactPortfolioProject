2c195c79d82f5ab328e9a25c8a93a6659bbfbccc
Felipe <fpizarro.ux@gmail.com>
2022-02-19 18:27:57 +0100

    newimgUpdate1902

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,7 +27,7 @@ function Contactcomponent () {
 
                         </div>
                         <div class="col-sm-4  mt-4" >
-                        <h1 className="textstrong mb-3 d-flex justify-content-center">Contact</h1>
+                            <h1 className="textstrong mb-3 d-flex justify-content-center">Contact</h1>
                             <p className="d-flex justify-content-center mb-1">I'm available for work! Contact me at </p><a href="mailto:fpizarro.ux@gmail.com" className="textstrong d-flex justify-content-center mb-1">fpizarro.ux@gmail.com</a>
                             <p className="d-flex justify-content-center mt-2 mb-1">See my code on&nbsp;<a className="textstrong" href="https://github.com/felipepizarro1" target="_blank">  GitHub</a></p>
                             <p className="d-flex justify-content-center ">Let's connect on&nbsp; <a className="textstrong" href="https://www.linkedin.com/in/felipepizarrodev" target="_blank">LinkedIn</a></p>
